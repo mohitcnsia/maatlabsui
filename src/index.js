2e337d8c066ed8de28c865686da1cb480d53284a
mohitkumar <mohitkumar+cnsia@gmail.com>
2024-05-11 15:18:14 +0530

    Learning React fundamentals

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const books = [
+  {
+    author: "Eobert t.kiyosaki",
+    title: "Rich Dad Poor Dad",
+    img: "https://m.media-amazon.com/images/I/41RMd71K3SL._AC_UF226,226_FMjpg_.jpg",
+  },
+  {
+    author: "Joseph Nguyen",
+    title: "Don't Believe Everything You Think",
+    img: "https://m.media-amazon.com/images/I/715qi-cIbML._AC_UY218_.jpg",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+    <section className="booklist">
+
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input');
+  };
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input 
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0'}}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click me</button>
+    </section>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
